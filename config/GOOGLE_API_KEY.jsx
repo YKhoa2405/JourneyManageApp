c8e07010e23c1c24a9134521871d041a7b8a808c
Nguyễn Y Khoa <148611782+YKhoa2405@users.noreply.github.com>
2024-06-19 17:00:43 +0700

    Update GOOGLE_API_KEY.jsx

--- config/GOOGLE_API_KEY.jsx
+++ config/GOOGLE_API_KEY.jsx
@@ -1,3 +1,3 @@
-export const GOOGLE_API_KEY = 'ArvHYzlNC_zl-qapSPj9KUSjb17DNAmCTHf0Lv-_sWiptCT-R26Ss9wvW5n9ytMr'
-export const client_id_api = "SrRCzVYnhP2IsJuuhskP8A26TTi4Gi61QjWTT8wV"
-export const client_secret_api = "s76UJbB4WmYD3bTDjMzc9gj0HNJCJaz1o5l05IZXr4cjwIar33MwREUD2p9A7zttMBDbAhZHLpdNiSpumpKkkjApeyuDj8fsIRWZinruf003B9xpk3cgQpzcapxhuLMM"
+// export const GOOGLE_API_KEY = 'ArvHYzlNC_zl-qapSPj9KUSjb17DNAmCTHf0Lv-_sWiptCT-R26Ss9wvW5n9ytMr'
+// export const client_id_api = "SrRCzVYnhP2IsJuuhskP8A26TTi4Gi61QjWTT8wV"
+// export const client_secret_api = "s76UJbB4WmYD3bTDjMzc9gj0HNJCJaz1o5l05IZXr4cjwIar33MwREUD2p9A7zttMBDbAhZHLpdNiSpumpKkkjApeyuDj8fsIRWZinruf003B9xpk3cgQpzcapxhuLMM"
